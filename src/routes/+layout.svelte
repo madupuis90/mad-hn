4724d0cd0d71ff8f6b02cd1e7e8de1eba993d97c
mad <ma.dupuis.90@gmail.com>
2022-11-12 18:29:05 -0500

    Fixed some css issues on mobile - should probably use css framework for real site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,30 +5,43 @@
 	export let data: LayoutData;
 </script>
 
-<nav class="navmenu">
-	<a class="home" href="/news">
-		<h1>[MAD] Hacker News</h1>
-	</a>
-  {#each data.urls as url}
-    <a class="link" href="{url}">{url[0].toUpperCase() + url.slice(1)}</a>
-  {/each}
-</nav>
+<div class="header">
+  <a class="home" href="/news">
+    <h1>[MAD] Hacker News</h1>
+  </a>
+  <nav class="navmenu">
+    {#each data.urls as url}
+      <a class="navitem" href="{url}">{url[0].toUpperCase() + url.slice(1)}</a>
+    {/each}
+  </nav>
+
+</div>
 <hr>
 <slot />
 
 <style>
+  .header {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: baseline;
+    justify-content: stretch;
+    margin: 0 1em;
+    gap: 0 4em;
+    align-content: flex-start;
+  }
 	.navmenu {
 		display: flex;
-		align-items: center;
+    flex-direction: row;
+		align-items: baseline;
     gap: 2em;
-		height: 4em;
-    margin: 10px;
+    align-content: flex-start;
   } 
   .home {
     text-decoration: none;
   }
-  .link {
-    font-size: 1.2em;
+  .navitem {
+    font-size: 1.4em;
     color: var(--main-title-color);
   }
 </style>
